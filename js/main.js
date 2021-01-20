87ff523b5643757a8376c8788dcaf7f109f3ffcb
kijimaru01 <suravut2539@hotmail.com>
2021-01-20 17:44:21 +0700

    update logic bug clean code

--- js/main.js
+++ js/main.js
@@ -1,7 +1,10 @@
 let count = 0;
-let boardGame = new Array();
+var boardGame = new Array();
 //create array
 function createBoard() {
+  count = 0;
+  document.getElementById("content2").style.display = "block";
+  document.getElementById("turnScreen").innerHTML = "Turn: Player1";
   var number = document.getElementById("nTable").value;
   var n = parseInt(number);
   for (let i = 0; i < number; i++) {
@@ -17,10 +20,51 @@ function createBoard() {
 
 function ClickTable(id) {
   count = count + 1;
+  let str = id.split(",");
+  let i = parseInt(str[0]);
+  let j = parseInt(str[1]);
   if (count % 2 == 1) {
     document.getElementById(id).innerHTML = "X";
+    document.getElementById("turnScreen").innerHTML = "Turn: Player2";
+    boardGame[i][j] = 1;
   } else {
     document.getElementById(id).innerHTML = "O";
+    document.getElementById("turnScreen").innerHTML = "Turn: Player1";
+    boardGame[i][j] = -1;
+  }
+  checkWinGame();
+}
+
+function checkWinGame() {
+  var number = document.getElementById("nTable").value;
+  let checkDiagonal1 = 0;
+  let checkDiagonal2 = 0;
+  for (let i = 0; i < number; i++) {
+    let checkHorizontal = 0;
+    let checkVertical = 0;
+    for (let j = 0; j < number; j++) {
+      checkHorizontal = checkHorizontal + boardGame[i][j];
+      checkVertical = checkVertical + boardGame[j][i];
+      if (i == j) {
+        checkDiagonal1 = checkDiagonal1 + boardGame[i][j];
+      }
+      if (i + j == number - 1) {
+        checkDiagonal2 = checkDiagonal2 + boardGame[i][j];
+      }
+      if (
+        checkHorizontal == -number ||
+        checkHorizontal == number ||
+        checkVertical == -number ||
+        checkVertical == number ||
+        checkDiagonal1 == -number ||
+        checkDiagonal1 == number ||
+        checkDiagonal2 == -number ||
+        checkDiagonal2 == number
+      ) {
+        alert("Win");
+        createTable();
+      }
+    }
   }
 }
 
